710977fb425cf2385c36f905ea5709b0cb3cdb89
yangxr <yangxr1995@gmail.com>
2024-05-28 18:40:56 +0800

    first commit

---
+++ lua/user/tree.lua
@@ -0,0 +1,49 @@
+local status_ok, tree = pcall(require, "nvim-tree")
+if not status_ok then
+  return
+end
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- 打开 command 快捷键映射
+local opts = { noremap = true, silent = true }
+local api = require "nvim-tree.api"
+vim.keymap.set('n', '<F2>', api.tree.toggle, opts)
+
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+  vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+
+end
+
+tree.setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+  on_attach = my_on_attach,
+})
+
