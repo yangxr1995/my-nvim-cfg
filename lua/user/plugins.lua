58b06cd23fbf17c57d82d9feeb49382ec05f5c8b
yangxr <yangxr1995@gmail.com>
2024-06-03 12:54:37 +0800

    add todo

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -222,6 +222,9 @@ return packer.startup(function(use)
     end
   }
 
+  -- TODO
+  use 'folke/todo-comments.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
