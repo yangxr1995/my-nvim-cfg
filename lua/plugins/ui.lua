2b4a524446d63f8d6e496647960bd9999301e73b
yangxr <yangxr1995@gmail.com>
2024-09-29 00:08:28 +0800

    some

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,8 +1,70 @@
 local config = require "luasnip.config"
 return {
     {
+        'nvim-tree/nvim-web-devicons',
+        config = function()
+            require 'nvim-web-devicons'.setup {
+                -- your personnal icons can go here (to override)
+                -- you can specify color or cterm_color instead of specifying both of them
+                -- DevIcon will be appended to `name`
+                override = {
+                    zsh = {
+                        icon = "",
+                        color = "#428850",
+                        cterm_color = "65",
+                        name = "Zsh"
+                    }
+                },
+                -- globally enable different highlight colors per icon (default to true)
+                -- if set to false all icons will have the default icon's color
+                color_icons = true,
+                -- globally enable default icons (default to false)
+                -- will get overriden by `get_icons` option
+                default = true,
+                -- globally enable "strict" selection of icons - icon will be looked up in
+                -- different tables, first by filename, and if not found by extension; this
+                -- prevents cases when file doesn't have any extension but still gets some icon
+                -- because its name happened to match some extension (default to false)
+                strict = true,
+                -- set the light or dark variant manually, instead of relying on `background`
+                -- (default to nil)
+                variant = "light|dark",
+                -- same as `override` but specifically for overrides by filename
+                -- takes effect when `strict` is true
+                override_by_filename = {
+                    [".gitignore"] = {
+                        icon = "",
+                        color = "#f1502f",
+                        name = "Gitignore"
+                    }
+                },
+                -- same as `override` but specifically for overrides by extension
+                -- takes effect when `strict` is true
+                override_by_extension = {
+                    ["log"] = {
+                        icon = "",
+                        color = "#81e043",
+                        name = "Log"
+                    }
+                },
+                -- same as `override` but specifically for operating system
+                -- takes effect when `strict` is true
+                override_by_operating_system = {
+                    ["apple"] = {
+                        icon = "",
+                        color = "#A2AAAD",
+                        cterm_color = "248",
+                        name = "Apple",
+                    },
+                },
+            }
+        end
+    },
+    {
         'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        dependencies = {
+            { 'nvim-tree/nvim-web-devicons', opt = true }
+        },
         config = function()
             require('lualine').setup {
                 options = {
@@ -48,7 +110,11 @@ return {
     },
     {
         'akinsho/bufferline.nvim',
-        dependencies = 'nvim-tree/nvim-web-devicons',
+        dependencies =
+        {
+            { 'nvim-tree/nvim-web-devicons', opt = true }
+        },
+
         config = function()
             vim.opt.termguicolors = true,
                 require("bufferline").setup {}
@@ -114,7 +180,6 @@ return {
 
         'kevinhwang91/nvim-bqf', ft = 'qf'
     },
-
     {
         'rmagatti/goto-preview',
         config = function()
@@ -161,5 +226,7 @@ return {
             }
         end
     },
-
+    {
+        "echasnovski/mini.icons",
+    },
 }
