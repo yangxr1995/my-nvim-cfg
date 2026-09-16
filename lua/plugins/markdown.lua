bec7fc85e3317ad26b325c4b7e32c4232a7d6464
yangxr <yangxr1995@gmail.com>
2026-09-16 15:54:48 +0800

    chore: 移除 md-render.nvim 渲染方案
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,28 +1,5 @@
 return {
     {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = "cd app && yarn install",
-        init = function()
-            vim.g.mkdp_filetypes = { "markdown" }
-        end,
-        ft = { "markdown" },
-    },
-    {
-        'MeanderingProgrammer/render-markdown.nvim',
-        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-        ---@module 'render-markdown'
-        ---@type render.md.UserConfig
-        ft = { "markdown", "codecompanion" },
-        config = function ()
-            require("render-markdown").setup({
-                latex = {enabled = false},
-            })
-        end
-    },
-    {
         "HakonHarnes/img-clip.nvim",
         -- event = "VeryLazy",
         ft = { "markdown" },
